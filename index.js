a14d5e282d2b46e9d9f917fb1668a07596247207
Jules <jules.vandewiele@gmail.com>
2020-05-10 18:33:15 +0200

    solve issues

--- index.js
+++ index.js
@@ -8,19 +8,27 @@ const PORT = process.env.PORT || 5000
 
 const HEROKU_LINK = "";
 
+/*app.use((req, res, next) => {
+    if(req.url === "/favicon"){
+        res.status(204).end("no favicon");
+    }else{
+        next();
+    }
+})*/
+
+app.use("favicon", (req, res, next) => {
+    console.log("favicon");
+}),
+
 app.use("/", (req, res, next) => {
     console.log("Request url: " + req.url);
-    fs.stat(path.join(__dirname, "dist"), (err, stats) => {
+    fs.stat(path.join(path.join(__dirname, "dist"), "online-cv"), (err, stats) => {
         if(err) res.status(500).end("Error, no dist folder detected.");
         else next();
     });
 });
 
-app.use("/", express.static(path.join(__dirname, "dist")));
-
-app.get("/**", (req, res) => {
-    res.status(500).end("Error, please try this link: \r\n" + HEROKU_LINK);
-});
+app.use("/", express.static(path.join(path.join(__dirname, "dist"), "online-cv")));
 
 app.listen(PORT, () => {
     console.log("Server started on port " + PORT);
