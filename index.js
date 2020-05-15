29c16384a3e2bb4a7f86f7c4e995320b73105f8d
Jules <jules.vandewiele@gmail.com>
2020-05-15 17:19:14 +0200

    solve directive issue, add few things

--- index.js
+++ index.js
@@ -8,18 +8,6 @@ const PORT = process.env.PORT || 5000
 
 const HEROKU_LINK = "";
 
-/*app.use((req, res, next) => {
-    if(req.url === "/favicon"){
-        res.status(204).end("no favicon");
-    }else{
-        next();
-    }
-})*/
-
-/*app.use("favicon", (req, res, next) => {
-    console.log("favicon");
-}),*/
-
 app.use("/", (req, res, next) => {
     //console.log("Request url: " + req.url);
     fs.stat(path.join(path.join(__dirname, "dist"), "online-cv"), (err, stats) => {
